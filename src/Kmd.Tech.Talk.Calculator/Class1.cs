d7185e078b15dc9693202a1f1a6b2f530a32a504
Dileep Dayanand Pai <dilludp123@gmail.com>
2019-08-08 15:28:51 +0530

    2nd commit

---
+++ src/Kmd.Tech.Talk.Calculator/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kmd.Tech.Talk.Calculator
+{
+    public class Class1 : IAddition
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ src/Kmd.Tech.Talk.Calculator/IAddition.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kmd.Tech.Talk.Calculator
+{
+    public interface IAddition
+    {
+        int Add(int a, int b);
+    }
+}
